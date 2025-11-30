65f10bfc774f732479283d8c18ebe2a278c1515c
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2025-11-30 16:39:45 +0000

    Message(e.g., "Added Second largest element progrm")

--- SecondLargestElement.java
+++ SecondLargestElement.java
@@ -2,11 +2,24 @@ import java.util.*;
 public class SecondLargestElement{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++){
+        int a=sc.nextInt();
+        int[] arr=new int[a];
+        for(int i=0;i<a;i++){
             arr[i]=sc.nextInt();
         }
+        int largest=Integer.MIN_VALUE;
+        int second=Integer.MIN_VALUE;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i]>largest){
+                second=largest;
+                largest=arr[i];
+            }
+            else if(arr[i]<largest && arr[i]>second){
+                second=arr[i];
+            }
+        }
+        System.out.println(second);
     }
 
 }
