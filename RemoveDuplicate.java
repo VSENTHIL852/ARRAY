f937667c28b44fa54091490a92b8896171be2247
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2025-12-05 15:13:55 +0000

    Message(add remove dup)

---
+++ RemoveDuplicate.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class RemoveDuplicate{
+    public static HashSet<Integer> ret(int[] arr){
+        int n=arr.length;
+        HashSet<Integer> s=new HashSet<>();
+        for(int i=0;i<n;i++){
+            s.add(arr[i]);
+        }
+        return s;
+
+    }
+public static void main(String[] args){
+     int[] arr = {1, 2, 2, 3, 4, 4, 4, 5, 5};
+        HashSet<Integer> s = ret(arr);    
+     for(int x:s){
+        System.out.print(x+" ");
+     }
+}
+}
