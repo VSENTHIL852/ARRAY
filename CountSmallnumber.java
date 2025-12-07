6afc74441af4f32d052e92e0434678db253a68c6
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2025-12-07 09:24:22 +0000

    Message(add Count Small number in array by current number)

---
+++ CountSmallnumber.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class CountSmallnumber{
+    static int[] ret(int[] arr){
+        int n=arr.length;
+        int[] m=new int[n];
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<n;j++){
+                if(arr[j]<arr[i]){
+                    count++;
+                }
+            }
+            m[i]=count;
+        }
+        return m;
+    }
+    public static void main(String[] args){
+        int[] arr={6,5,4,8};
+        int[] m=ret(arr);
+        for(int x:m){
+            System.out.print(x+" ");
+        }
+
+    }
+}
