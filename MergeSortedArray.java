56c3be4fbe5936388c1f20b3b904d4e88b10a9f1
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2025-12-10 22:24:47 +0530

    Create MergeSortedArray.java

---
+++ MergeSortedArray.java
@@ -0,0 +1,41 @@
+class MergeSortedArray{
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int[] temp = new int[m + n];   // temporary merged array
+        
+        int i = 0;   // pointer for nums1
+        int j = 0;   // pointer for nums2
+        int k = 0;   // pointer for temp
+
+        // merge from the FRONT
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                temp[k] = nums1[i];
+                i++;
+            } else {
+                temp[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+
+        // remaining elements of nums1
+        while (i < m) {
+            temp[k] = nums1[i];
+            i++;
+            k++;
+        }
+
+        // remaining elements of nums2
+        while (j < n) {
+            temp[k] = nums2[j];
+            j++;
+            k++;
+        }
+
+        // copy temp back to nums1
+        for (int x = 0; x < m + n; x++) {
+            nums1[x] = temp[x];
+        }
+    }
+}
