7b489387a4a28ed3fd0b960e25f7814de297508c
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2025-12-21 12:24:51 +0530

    Fix formatting issue in BuyandSell1.java

--- BuyandSell1.java
+++ BuyandSell1.java
@@ -10,7 +10,7 @@ public class BuyandSell1{
         return store;
     }
       public static void main(String[] args){
-        int[] arr={7, 10, 1, 3, 6, 9, 2};
+        int[] arr={7, 10, 1, 3, 6, 9, 2};   
         System.out.print(ret(arr));        
     }
 }
