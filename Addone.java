1884b3e0941d68d3afe2a6c54e965e9fda0293e8
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2025-12-24 20:25:55 +0530

    Update Addone.java

--- Addone.java
+++ Addone.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Addone{
     public static void main(String[] args){
     int[] arr={9,9,9};
-        int a=0; 
+        int a=0;  
     for(int i=0;i<arr.length;i++){
         int b=arr[i];
         a=a*10+b;
