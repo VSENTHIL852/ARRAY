86cde2cdccf35ed3bfc6133be970211c6ac2bf6b
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2026-01-02 22:04:33 +0530

    Update CountSmallnumber.java

--- CountSmallnumber.java
+++ CountSmallnumber.java
@@ -4,10 +4,10 @@ public class CountSmallnumber{
         int n=arr.length;
         int[] m=new int[n]; 
         for(int i=0;i<n;i++){
-            int count=0;
+            int count=0; 
             for(int j=0;j<n;j++){
                 if(arr[j]<arr[i]){
-                    count++;
+                    count++; 
                 }
             }
             m[i]=count;
